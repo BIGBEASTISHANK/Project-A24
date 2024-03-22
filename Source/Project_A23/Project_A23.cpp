e8a5f0f352e7cf45e84d61dabc601c4af3e5cdaf
BIGBEASTISHANK <ishank@bigbeastishank.com>
2024-03-22 08:10:30 +0530

    Initial Commit

---
+++ Source/Project_A23/Project_A23.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_A23.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_A23, "Project_A23" );
